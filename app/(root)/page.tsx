ec17ae01b3ba3d44dd9b485939716b07260ae6f4
Jin Yang <ginyang6@gmail.com>
2026-04-04 20:28:35 -0400

    added header component (left side) and layout structure

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+//represents our homepage!!
+
+import Image from "next/image";
+import Header from "@/components/organisms/header";
+
+
+export default function Home() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
---
+++ components/organisms/header.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import { Plus, Sun } from "lucide-react";
+
+export default function Header() {
+  return (
+    <header className="w-full bg-background-2 sticky top-0 z-50">
+      <div className="max-w-[1440px] h-[65px] mx-auto px-6 md:px-8 flex items-center justify-between">
+        {/* Left Side: Logo & Brand */}
+        <div className="flex items-center gap-3">
+          <Link href="/" className="flex items-center gap-3">
+            {/* Medical Cross Icon */}
+            <div className="bg-[#1B9AF5] w-8 h-8 rounded-sm flex items-center justify-center text-white shrink-0">
+              <Plus size={24} strokeWidth={3} />
+            </div>
+            {/* Brand Name */}
+            <h3 className="hidden lg:block">CareSync Medical Center</h3>
+          </Link>
+        </div>
+
+        {/*  Right Side  */}
+        <div>
+          
+        </div>
+      </div>
+    </header>
+  );
+}

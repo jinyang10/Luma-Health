8a978d71cd3e7eda58dce71f116e5d5519fc9caf
Jin Yang <ginyang6@gmail.com>
2026-04-28 21:18:38 -0400

    desktop view right header hover more visible on theme toggle button

--- components/molecules/mobile-user-signinoravatar.tsx
+++ components/molecules/mobile-user-signinoravatar.tsx
@@ -4,10 +4,12 @@
 
 import InteractiveSignInButton from "./interactive-sign-in-button";
 
+// defining props interface, define an optional prop called onMobileActionComplete
 interface MobileUserMenuProps {
     onMobileActionComplete?: ()=> void;
 }
 
+// extract prop onMobileActionComplete from the props object 
 export default function MobileUserSignOrAvator({onMobileActionComplete}: MobileUserMenuProps) {
     return (
         // in order to pass the function (which closes the sidebar) down, must receive it as a prop from menu-client
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -15,7 +15,7 @@ const buttonVariants = cva(
         secondary:
           "rounded-md bg-background-2 border border-border-2 body-regular hover:bg-background-2 hover:opacity-50",
         ghost:
-          "hover:bg-muted hover:text-foreground aria-expanded:bg-muted aria-expanded:text-foreground dark:hover:bg-muted/50",
+          "hover:bg-background hover:text-foreground aria-expanded:bg-muted aria-expanded:text-foreground dark:hover:bg-muted/50",
         destructive:
           "bg-destructive/10 text-destructive hover:bg-destructive/20 focus-visible:border-destructive/40 focus-visible:ring-destructive/20 dark:bg-destructive/20 dark:hover:bg-destructive/30 dark:focus-visible:ring-destructive/40",
         link: "text-primary underline-offset-4 hover:underline",

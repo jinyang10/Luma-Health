ae1112e888bb7a624a8c657ead33b228d5ed5018
Jin Yang <ginyang6@gmail.com>
2026-04-28 20:30:48 -0400

    added styling to right header mobile menu

--- components/molecules/menu-client.tsx
+++ components/molecules/menu-client.tsx
@@ -5,11 +5,12 @@ import { useRouter } from "next/navigation";
 import { EllipsisVertical, MoreVertical } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import {
-  Sheet,
-  SheetContent,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
+    Sheet,
+    SheetContent,
+    SheetFooter,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
 } from "@/components/ui/sheet";
 import { ThemeToggle } from "./theme-toggle";
 import { useState } from "react";
@@ -21,18 +22,18 @@ interface MenuClientProps {
 }
 
 // define component for right side of header
-export default function MenuClient({desktopAvatar} : MenuClientProps) {
+export default function MenuClient({ desktopAvatar }: MenuClientProps) {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
-    const closeMobileMenu = ()=> {
+    const closeMobileMenu = () => {
         setIsMobileMenuOpen(false);
 
     };
-    
+
     // by default: when you open/close the side sheet, component returns focus to the element that originally opened the sheet
     // if you don't prevent default behavior, when user clicks on trigger to close it, library automatically shifts 
     // browser's focus back to the 3 dots --> results in seeing a blue/black outline around the trigger button
-    const handleSheetCloseAutoFocus = (event:Event)=> {
+    const handleSheetCloseAutoFocus = (event: Event) => {
         event.preventDefault();
     };
 
@@ -41,16 +42,16 @@ export default function MenuClient({desktopAvatar} : MenuClientProps) {
     const handleBookAppointment = () => router.push("/");
     const handleSignIn = () => router.push("/sign-in");
 
-    return(
+    return (
         <div>
             { /* Desktop, Tablet Nav */}
             <nav className="hidden md:flex items-center gap-3">
                 {/* Theme Toggle */}
                 <ThemeToggle />
-                
+
                 {/* Home Link */}
-                <Link href="/" className="body-regular text-text-body hover:text-primary"> 
-                Home 
+                <Link href="/" className="body-regular text-text-body hover:text-primary">
+                    Home
                 </Link>
 
                 {/* Book Appointment Button */}
@@ -72,59 +73,60 @@ export default function MenuClient({desktopAvatar} : MenuClientProps) {
 
                 {/* when user clicks on trigger to open/close the sheet, we call setter function and pass value true/false to it
                 and isMobileMenuOpen's value is updated */}
-                {/* this is a Controlled Component (React state controls Sheet's open/closed status/value) */}
+
+                {/* ** SHEET: a CONTROLLED COMPONENT (React state controls Sheet's open/closed status/value) */}
                 <Sheet open={isMobileMenuOpen} onOpenChange={setIsMobileMenuOpen}>
 
                     {/* Trigger: determines what controls the opening of the sheet: 3 vertical dots */}
 
                     {/* when you click on the button, sheettrigger captures the click and tells parent sheet to change its 
                     internal state to open --> renders the sheet content*/}
-                    <SheetTrigger className="align-middle" onClick={()=>setIsMobileMenuOpen(true)}>
-                        
+                    <SheetTrigger className="align-middle" onClick={() => setIsMobileMenuOpen(true)}>
+
                         {/* the 3 vertical dots (EllipsisVertical) is the clickable element for SheetTrigger */}
                         <EllipsisVertical />
                     </SheetTrigger>
-                    
+
                     {/* Content: Sidebar */}
-                    <SheetContent 
+                    <SheetContent
                         // side="right" means the sheetcontent (sidebar) will appear on the right side
-                        side="right" 
+                        side="right"
                         onCloseAutoFocus={handleSheetCloseAutoFocus}
                         className="flex flex-col items-start p-4 bg-bakcground-2"
                     >
-                        <SheetHeader className="border-b border-stroke-1 pb-4 text-left">
-                            <SheetTitle>Menu</SheetTitle>
+                        <SheetHeader className="w-full justify-start flex flex-row items-center border-b ">
+                            <SheetTitle className="text-2xl font-bold">Menu</SheetTitle>
                         </SheetHeader>
 
-                        {/* Top Items */}
-                            
-                                <ThemeToggle />
+                        {/* Top Items below Menu header */}
+                        <ThemeToggle />
 
-                            <Link 
-                                href="/" 
-                                className="body-regular text-text-body hover:text-primary transition-colors"
-                            >
-                                Home
-                            </Link>
+                        <Link
+                            href="/"
+                            className="body-regular text-text-body hover:text-primary py-3"
+                            // add event listener to close the sidebar when this link is pressed
+                            onClick={closeMobileMenu}
+                        >
+                            Home
+                        </Link>
 
-                            <Button 
-                                variant="brand" 
-                                className="w-full" 
-                                onClick={handleBookAppointment}
-                            >
-                                Book Appointment
-                            </Button>
+                        <Button
+                            variant="brand"
+                            size="sm"
+                            className="w-full text-text-caption-2 my-2"
+                            onClick={()=> {
+                                closeMobileMenu(); 
+                                handleBookAppointment();
+                            }}
+                        >
+                            Book Appointment
+                        </Button>
 
-                        {/* Bottom Pinned Item */}
-                        <div className="mt-auto pb-4">
-                            <Button 
-                                variant="secondary" 
-                                className="w-full bg-bg-2 hover:bg-bg-3 text-text-body" 
-                                onClick={handleSignIn}
-                            >
-                                Sign In
-                            </Button>
-                        </div>
+                        {/* Render the desktopAvatar prop here for the Signin component*/}
+                        <SheetFooter className="w-full">
+                            {desktopAvatar}
+                        </SheetFooter>
+
                     </SheetContent>
                 </Sheet>
             </nav>
